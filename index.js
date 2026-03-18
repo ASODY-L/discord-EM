807f14ec5f661d06d92add66b2b44d2ffe8e1a54
ASODY-L <asodi.bae@gmail.com>
2026-03-18 08:41:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const TOKEN = process.env.TOKEN;
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  // 🔊 أمر دخول
+  if (message.content === "/join") {
+    const channel = message.member.voice.channel;
+
+    if (!channel) {
+      return message.reply("ادخل روم صوتي اول 😅");
+    }
+
+    joinVoiceChannel({
+      channelId: channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
+
+    message.reply("دخلت الروم 🔊");
+  }
+
+  // ❌ أمر خروج
+  if (message.content === "/leave") {
+    const connection = getVoiceConnection(message.guild.id);
+
+    if (!connection) {
+      return message.reply("انا اصلاً مو داخل 😅");
+    }
+
+    connection.destroy();
+    message.reply("طلعت من الروم 👋");
+  }
+});
+
+client.login(TOKEN);
